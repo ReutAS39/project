22942e38b66e6846b288a78560f7ae35ed98e3a0
ReutAS <reutas39@gmail.com>
2022-12-27 16:38:24 +0200

    simpleapp D6 fin

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'simpleapp',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
